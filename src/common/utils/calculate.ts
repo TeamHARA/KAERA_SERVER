c5504241fd38eee4b750373cc7d958b952cdd6ed
leGit-y <sky3nlq@gmail.com>
2023-08-04 20:53:24 +0900

    [FEAT] utils에 계산로직 빼기

---
+++ src/common/utils/calculate.ts
@@ -0,0 +1,18 @@
+   
+const calculate_Dday =async (deadlineDate:Date) => {
+    const moment = require('moment');
+    //d-day 계산
+    let gap = -888;                   // (데드라인 존재하지 않을 경우) : gap = -1
+    if (deadlineDate != null){      // (데드라인 존재하는 경우) : gap = d-day에 해당하는 값
+        const today = moment(moment().format('YYYY-MM-DD'));        // d-day 계산 (날짜 차이 계산을 위해 today 와 deadline을 moment 객체로 만들어줌)
+        const deadline = moment(moment(deadlineDate).format('YYYY-MM-DD'));
+        gap = today.diff(deadline, 'days')
+    }
+
+    return gap;
+}  
+
+export {
+    
+}
+   
--- src/service/worryService.ts
+++ src/service/worryService.ts
@@ -85,18 +85,14 @@ const getWorryDetail =async (worryId: number,userId: number) => {
         throw new ClientException("고민글 작성자만 조회할 수 있습니다.");
     }
 
-    let gap;
-    // (데드라인 존재하는 경우) : gap = d-day에 해당하는 값
-    if (worry.deadline != null){
+    let gap = -888;                      // (데드라인 존재하지 않을 경우) : gap = -888
+    if (worry.deadline != null){         // (데드라인 존재하는 경우) : gap = d-day에 해당하는 값
         // d-day 계산 (날짜 차이 계산을 위해 today 와 deadline을 moment 객체로 만들어줌)
         const today = moment(moment().format('YYYY-MM-DD'));
         const deadline = moment(moment(worry.deadline).format('YYYY-MM-DD'));
-        gap = deadline.diff(today, 'days')
+        gap = today.diff(deadline, 'days')
     }
-    // (데드라인 존재하지 않을 경우) : gap = -1
-    else{
-        gap = -1;
-    }
+   
 
     // local time = kst(korean standard time) 는 utc 기준 +9시간이므로 offset 9로 설정
     const kst_created_at = moment(worry.created_at).utc().utcOffset(9).format('YYYY-MM-DD');
@@ -157,11 +153,11 @@ const patchDeadline =async (deadlineUpdateDTO: deadlineUpdateDTO) => {
     }
 
     //d-day 계산
-    let gap = -1;                   // (데드라인 존재하지 않을 경우) : gap = -1
+    let gap = -888;                   // (데드라인 존재하지 않을 경우) : gap = -1
     if (deadlineDate != null){      // (데드라인 존재하는 경우) : gap = d-day에 해당하는 값
         const today = moment(moment().format('YYYY-MM-DD'));        // d-day 계산 (날짜 차이 계산을 위해 today 와 deadline을 moment 객체로 만들어줌)
         const deadline = moment(moment(deadlineDate).format('YYYY-MM-DD'));
-        gap = deadline.diff(today, 'days')
+        gap = today.diff(deadline, 'days')
     }
 
 
