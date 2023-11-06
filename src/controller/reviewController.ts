5df7b67a25273ad41a236108f30f159b94ad7bb3
leGit-y <sky3nlq@gmail.com>
2023-11-06 23:13:40 +0900

    [FIX] put->patch 및 리턴값 수정

--- src/controller/reviewController.ts
+++ src/controller/reviewController.ts
@@ -5,18 +5,19 @@ import statusCode from "../constants/statusCode";
 import { reviewDTO } from "../interfaces/DTO/reviewDTO"
 import reviewService from "../service/reviewService";
 
-const putReview = async (req: Request, res: Response, next: NextFunction) => {
+const patchReview = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const reviewDTO: reviewDTO = req.body;
-        const data = await reviewService.putReview(reviewDTO);
-        let message = null;
-        if(!data)
+        const data = await reviewService.patchReview(reviewDTO);
+        
+        let message;
+        if(data.isNew)
             message = rm.CREATE_REVIEW_SUCCESS
         else
             message = rm.UPDATE_REVIEW_SUCCESS
 
 
-        return res.status(sc.OK).send(success(statusCode.OK, message, data));
+        return res.status(sc.OK).send(success(statusCode.OK, message, data.result));
 
     } catch (error) {
         next(error);
@@ -24,5 +25,5 @@ const putReview = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 export default{
-    putReview
+    patchReview
 }
