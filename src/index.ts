6ea23ce4036098f1078f545189a95c4508066df9
leGit-y <sky3nlq@gmail.com>
2023-11-17 15:37:06 +0900

    [TEST] cd 잘 되는지

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import App from './app';
 
 const app = new App().setup();
 
-
-const port = 3000; // 사용할 port를 3000번으로 설정
+const port = 3000; 
 app.listen(port || 3000, () => {
   console.log(`
         #############################################
