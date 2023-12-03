fb41052e6dfdcb462c64b06f5960b33cad53e101
leGit-y <sky3nlq@gmail.com>
2023-12-03 22:52:05 +0900

    [CHORE] alarm controller

--- src/controller/alarmController.ts
+++ src/controller/alarmController.ts
@@ -9,27 +9,27 @@ admin.initializeApp({
 
 const pushAlarm = (req: Request, res: Response, next: NextFunction) => {
     try{
-    const {deviceToken} = req.body;
+        const { deviceToken } = req.body;
 
-    let message = {
-        notification: {
-        title: '테스트 발송',
-        body: '우와~!!!!!!',
-        },
-        token: deviceToken,
-    }
+        let message = {
+            notification: {
+            title: '테스트 발송',
+            body: '우와~!!!!!!',
+            },
+            token: deviceToken,
+        }
 
-    admin
-        .messaging()
-        .send(message)
-        .then(function (response:Response) {
-        console.log('Successfully sent message: : ', response)
-        return res.status(200).json({success : true})
-        })
-        .catch(function (err:Error) {
-            console.log('Error Sending message!!! : ', err)
-            return res.status(400).json({success : false})
-        });
+        admin
+            .messaging()
+            .send(message)
+            .then(function (response:Response) {
+            console.log('Successfully sent message: : ', response)
+            return res.status(200).json({success : true})
+            })
+            .catch(function (err:Error) {
+                console.log('Error Sending message!!! : ', err)
+                return res.status(400).json({success : false})
+            });
     } catch (error) {
         next(error);
     }
