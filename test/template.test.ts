07c84056a07fb6f6a95a82664df096ee85c7b2cb
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-02-02 00:34:29 +0900

    [TEST] template test

--- test/template.test.ts
+++ test/template.test.ts
@@ -2,82 +2,8 @@ import { prismaMock } from './singleton';
 import templateService from "../src/service/templateService";
 import templateRepository from '../src/repository/templateRepository';
 import { userService } from '../src/service';
+import { ClientException } from '../src/common/error/exceptions/customExceptions';
 
-
-// test('should read all templates ', async () => {
-  
-//   const newUser = {
-//     name: 'Rich',
-//     email: 'hello@prisma.io',
-//     kakaoId: 2020200,
-//     appleId: "apple_id",
-//     ageRange: "20~29",
-//     gender: "male",
-//     refreshToken: "refresh_token",
-//     deviceToken: "device_token",
-//   }
-
-//   const createdNewUser = {
-//     id:1,
-//     ...newUser,
-//     used_template:[1,2],
-//     created_at: new Date(),
-//     updated_at: new Date(),
-//     kakao_id: BigInt(2020200),
-//     apple_id: "apple_id",
-//     age_range: "20~29",
-//     gender: "male"
-//   }
-
-//   // mock으로 가짜 유저 생성
-//   prismaMock.user.create.mockResolvedValue(createdNewUser);
-
-
-//   const templates = [
-//     {
-//       id: 1,
-//       info: "template_1_info",
-//       title: "template_1_title",
-//       shortInfo: "emplate_1_shortInfo",
-//       questions: ["질문1", "질문2","질문3"],
-//       guideline: "template_1_guideline",
-//       hints: ["힌트1", "힌트2","힌트3"],
-//       subtitles: ["소제목1", "소제목2","소제목3"]
-//     },
-//     {
-//       id: 2,
-//       info: "template_2_info",
-//       title: "template_2_title",
-//       shortInfo: "emplate_2_shortInfo",
-//       questions: ["질문1", "질문2","질문3"],
-//       guideline: "template_2_guideline",
-//       hints: ["힌트1", "힌트2","힌트3"],
-//       subtitles: ["소제목1", "소제목2","소제목3"]
-//     }
-//   ]
-//   (templateRepository.findAllTemplate as jest.Mock).mockResolvedValue(templates);
-
-//   const result = [
-//     {
-//       templateId: templates[0].id,
-//       title: templates[0].title,
-//       shortInfo: templates[0].shortInfo,
-//       info: templates[0].info,
-//       hasUsed: createdNewUser.used_template.includes(templates[0].id),
-//     },
-//     {
-//       templateId: templates[1].id,
-//       title: templates[1].title,
-//       shortInfo: templates[1].shortInfo,
-//       info: templates[1].info,
-//       hasUsed: createdNewUser.used_template.includes(templates[1].id),
-//     }
-//   ]
-  
-//   await expect(templateRepository.findAllTemplate()).resolves.toEqual(templates);
-//   await expect(templateService.getAllTemplate(createdNewUser.id)).resolves.toEqual(result);
-      
-// })
 
 describe('getAllTemplate', () => {
   it('should return all templates with hasUsed flag', async () => {
@@ -141,7 +67,47 @@ describe('getAllTemplate', () => {
       }
     ]);
   });
+});
 
 
+describe('getTemplateById', () => {
+  afterEach(() => {
+    // 각 테스트 후에 spyOn 리셋
+    jest.restoreAllMocks();
+  });
 
+  it('should return template data when template exists', async () => {
+    const mockTemplate = {
+      id: 1,
+      info: "template_1_info",
+      title: "template_1_title",
+      shortInfo: "emplate_1_shortInfo",
+      questions: ["질문1", "질문2","질문3"],
+      guideline: "template_1_guideline",
+      hints: ["힌트1", "힌트2","힌트3"],
+      subtitles: ["소제목1", "소제목2","소제목3"]
+    }
+    
+    // Repository를 mock 처리
+    prismaMock.template.findUnique.mockResolvedValue(mockTemplate);
+    
+    // Mock service behavior
+    const data = {
+      title: "template_1_title",
+      guideline: "template_1_guideline",
+      questions: ["질문1", "질문2","질문3"],
+      hints: ["힌트1", "힌트2","힌트3"]
+    }
+    jest.spyOn(templateService, 'getTemplateById').mockResolvedValue(data);
+
+    const result = await templateService.getTemplateById(1);
+    expect(result).toEqual(data);
+
+  });
+
+  it('should throw ClientException when template is null', async () => {
+    // Repository가 null 반환하도록 mock 처리
+    prismaMock.template.findUnique.mockResolvedValue(null);
+    await expect(templateService.getTemplateById(1)).rejects.toThrow(ClientException);
+  });
 });
